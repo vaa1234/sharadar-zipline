22a8ef45c0ccc4aace4b440336ee83d86b97aca8
vaa1234 <62284466+vaa1234@users.noreply.github.com>
2022-05-25 11:16:40 +0300

    moved resipient to server side

--- sharadar/util/telegram.py
+++ sharadar/util/telegram.py
@@ -1,7 +1,9 @@
 import requests
 
-def notify_telegram(msg, recipient):
+def notify_telegram(msg):
 
-    data = {"to": recipient, "message": msg, "silent": False}
-    headers = {'X-Auth-Token': 'JH9f84ghrg', "content-type":"application/json", "User-Agent":"curl/7.68.0"}
-    requests.post("http://localhost:8080/msg", json=data, headers=headers)
+    data = {"message": msg, "silent": False}
+    headers = {"content-type":"application/json", "User-Agent":"curl/7.68.0"}
+    status = requests.post("http://localhost:8080/msg", json=data, headers=headers)
+
+    return status.ok
